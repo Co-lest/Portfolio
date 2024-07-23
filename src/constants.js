029a0421a9cf600fb00f3c1b3721ee32778bd5bf
Mark Tom <122617440+Co-lest@users.noreply.github.com>
2024-07-23 21:07:04 +0300

    main and constant

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript, react/C++/PHP and SQL.
+    Here is my <a href="https://github.com/Co-lest" target="_blank">Github</a> profile!`,
+  "cs-degree": `This is my High school degree, not yet graguated.`,
+  "sofa-table": `That's my sofa. I like to relax here and play games.`,
+  tv: `This is my tv. I like to play games and watch documentaries and programming tutorials.` ,
+  bed: `This where I sleep. And develop ideas.`,
+  resume:  `Contact me at markbironga@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn about Javascript using my repository in github <a href="https://github.com/Co-lest/Scriptlings">Github<a>`,
+  library: `I also have a repository for algorithms and data structures. i posted it on <a href="https://github.com/Co-lest/Data-structurers">Github<a>. If you have problems in it check it out, its pretty cool`,
+  exit: `If you want to exit this portfolio, just close the tab.`,
+};
